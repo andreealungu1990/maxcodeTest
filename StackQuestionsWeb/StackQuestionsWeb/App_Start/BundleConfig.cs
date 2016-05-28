ad52bdb785921f3f0c8c6dbedf002e767c28fa4d
andreealungu1990 <lungu.andreea1990@yahoo.com>
2016-05-28 16:43:19 +0300

    added mvc basic project

---
+++ StackQuestionsWeb/StackQuestionsWeb/App_Start/BundleConfig.cs
@@ -0,0 +1,21 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace StackQuestionsWeb
+{
+    public class BundleConfig
+    {
+        // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/js")
+                             .Include("~/Scripts/jquery-1.7.1.js")
+                             .Include("~/Scripts/jquery-ui-1.8.20.js")
+                             .Include("~/Scripts/bootstrap.js"));
+
+            bundles.Add(new StyleBundle("~/css")
+                          .Include("~/Content/bootstrap.css")
+                          .Include("~/Content/bootstrap-responsive.css"));
+        }
+    }
+}
