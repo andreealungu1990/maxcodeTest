346ef1fefde48031b7dd75668ea00c782ee440eb
andreealungu1990 <lungu.andreea1990@yahoo.com>
2016-05-28 17:23:18 +0300

    added models for api response and homecontroller with view

---
+++ StackQuestionsWeb/StackQuestionsWeb/Models/Item.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace StackQuestionsWeb.Models
+{
+    [DataContract]
+    public class Item
+    {
+        [DataMember(Name = "tags")]
+        public List<string> Tags { get; set; }
+      
+        [DataMember(Name = "view_count")]
+        public int ViewCount { get; set; }
+
+        [DataMember(Name = "answer_count")]
+        public int AnswerCount { get; set; }
+
+        [DataMember(Name = "score")]
+        public int Score { get; set; }
+
+        [DataMember(Name = "creation_date")]
+        public int CreationDate { get; set; }
+      
+        [DataMember(Name = "question_id")]
+        public int QuestionId { get; set; }
+
+        [DataMember(Name = "link")]
+        public string Link { get; set; }
+
+        [DataMember(Name = "title")]
+        public string Title { get; set; }
+
+        [DataMember(Name = "body")]
+        public string Body { get; set; }
+
+        [DataMember(Name = "share_link")]
+        public string ShareLink { get; set; }
+       
+    }
+}
---
+++ StackQuestionsWeb/StackQuestionsWeb/Models/RootObject.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace StackQuestionsWeb.Models
+{
+    [DataContract]
+    public class RootObject
+    {
+        [DataMember(Name = "items")]
+        public List<Item> Items { get; set; }
+        [DataMember(Name = "has_more")]
+        public bool HasMore { get; set; }
+        [DataMember(Name = "total")]
+        public int Total { get; set; }
+    }
+}
